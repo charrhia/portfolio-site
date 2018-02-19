dd3a877aff7abf43edb96e88b217bbceeb38977b
charrhia <charlotte.rhia.smith@gmail.com>
2018-02-19 16:14:32 -0500

    contact page

--- app/javascript/containers/ContactContainer.js
+++ app/javascript/containers/ContactContainer.js
@@ -11,11 +11,25 @@ class ContactContainer extends Component {
   render() {
 
     return(
-      <div>
-        Hello from the Contact Container
-      </div>
-    )
-  }
+      <div id="contact-me">
+        <p>Want to chat? Feel free to email me at
+          <a href="mailto:charlotte.smith93@gmail.com?Subject=Hello" target="_top"> charlotte.smith93@gmail.com</a>
+        </p>
+
+        <div className="icons">
+          <span>
+            <a href="https://github.com/charrhia">
+            <i className="fab fa-github"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/charlottersmith/">
+          <i className="fab fa-linkedin"></i>
+        </a>
+      </span>
+    </div>
+
+  </div>
+)
+}
 }
 
 export default ContactContainer;
