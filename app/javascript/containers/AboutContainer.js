5336c8efa1301a70c44ffa819d7af55283bfbe36
charrhia <charlotte.rhia.smith@gmail.com>
2018-02-18 23:25:51 -0500

    about page created

--- app/javascript/containers/AboutContainer.js
+++ app/javascript/containers/AboutContainer.js
@@ -11,8 +11,18 @@ class AboutContainer extends Component {
   render() {
 
     return(
-      <div>
-        Hello from the AboutContainer
+      <div id="about-me">
+        <p>Prior experience in human resources and influencer marketing in both global corporations and small scale start ups. I'm interested in making a career jump to IT so I enrolled in an immersive 10 week full-stack boot camp that focuses on the technologies required to enter the market as an entry-level developer</p>
+
+        <p>In my spare time, I love to cook, travel the world, and (write more) code.</p>
+        <ul>
+          Tech Skills:
+          <li>Ruby/Ruby on Rails</li>
+          <li>SQL/PostgreSQL</li>
+          <li>Javascript/React</li>
+          <li>HTML/CSS</li>
+        </ul>
+
       </div>
     )
   }
